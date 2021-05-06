3962ce1f884f850dfdfd4b96cfe5749fd76717ae
rpaulnarcis <rpaulnarcis@gmail.com>
2021-05-06 08:41:52 +0000

    Added bacic checkout functionality

--- furniture_plus/settings.py
+++ furniture_plus/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
-    #Other
+    # Other
     'crispy_forms',
 ]
 
@@ -175,3 +175,10 @@ MEDIA_URL = '/media/'
 
 FREE_DELIVERY_THRESHOLD = 200
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
